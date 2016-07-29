5feccc50aff23f4747dfc7e8c08e0c79f3a1da81
Nicolas Charvoz <nicolas.charvoz@epitech.eu>
2016-07-29 15:35:00 +0200

    new cloud code code

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,11 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.beforeSave('Events', function(req, res) {
+    if (req.object.get("title") == "title") {
+        res.error("You cannot create an event with the title: 'title' ")
+    } else {
+        res.success()
+    }
+});

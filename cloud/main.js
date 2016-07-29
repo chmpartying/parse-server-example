323a1b05a71a6cdf0f588dd2168aeffc407aeaff
Nicolas Charvoz <nicolas.charvoz@epitech.eu>
2016-07-29 16:10:32 +0200

    j'suis nul en js

--- cloud/main.js
+++ cloud/main.js
@@ -5,18 +5,18 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.beforeSave('Events', function(req, res) {
 
-    var badWords = ["merde", "bite"];
+    var badWords = ['merde', 'bite'];
     var check = true;
     var detectedWord = "";
-    var title = req.object.get("title");
+    var title = req.object.get('title');
 
-    for word in badWords {
+    for (word in badWords) {
         if (title.indexOf(word) != -1) {
             check = false;
             detectedWord = word;
         }
     }
-    
+
     if (check = false) {
         res.error("You cannot create an event with the title: " + detectedWord);
     } else {
